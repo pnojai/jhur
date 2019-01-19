2f02e8c727e934ed402e6389e64cb2b2a08aa59e
PnoJai <pnojai@protonmail.com>
2019-01-19 12:02:46 -0500

    Completed rankall.R

--- rankall.R
+++ rankall.R
@@ -38,12 +38,62 @@ rankall <- function(outcome, num = "best") {
         
         ## Subset the hospitals that report an outcome.
         outcome_is_na <- is.na( fil[, outcome_col_pos] )
+        hospitals <- fil[!outcome_is_na, c(2, 7, outcome_col_pos)]
         
         ## For each state, find the hospital of the given rank
         ## Return a data frame with the hospital names and the
         ## (abbreviated) state name
+        ## Initialize an accumlator table. Define the columns by setting them to empty vectors.
+        ## You can't set the columns to NULL because that won't create a column.
+        hospitals_out <- data.frame(hospital = character(), state = character())
         
-        hospitals <- fil[!outcome_is_na, c(2, 7, outcome_col_pos)]
+        ## Get a list of the states. Do this with table() instead of split.
+        ## Eventually you will remember that strings default to factors in data frames. Please set that to false.
+        states <- as.data.frame( table( hospitals[2]), stringsAsFactors = FALSE)
+        colnames(states) <- c("state", "freq")
         
-        split(hospitals, hospitals[2])
+        ## Assign rank for each state.
+        for ( state in states$state ) {
+                # print(state)
+
+                # Pick off the hospitals for one state.
+                hospitals_1_state <- hospitals[ hospitals[2] == state, ]
+                
+                # Order them by outcome and hospital name. Ties in outcome are broken
+                # by the alphabetic ordering of the hospital name.
+                hospitals_ordered <- order( hospitals_1_state[3], hospitals_1_state[1] )
+                hospitals_1_state <- hospitals_1_state[ hospitals_ordered, ]
+                
+                # Rank them by appending a sequence column.
+                hospitals_1_state$rank <- 1:nrow(hospitals_1_state)
+                
+                # Set the target rank based on the num parameter.
+                if (num == "best") {
+                        hospital_rank <- min(hospitals_1_state$rank)
+                } else if (num == "worst") {
+                        hospital_rank <- max(hospitals_1_state$rank)
+                } else {
+                        hospital_rank <- num
+                }
+                
+                # Get the output for the target rank.
+                # Initializing the output data frame is unnecessary because each loop iteration reassigns it.
+                # Also, frustratingly, the assignment changes the column names anyway. Might as well assign
+                # the output and then assign the names all over again.
+                hospitals_1_state_output <- hospitals_1_state[ hospitals_1_state$rank == hospital_rank, 1:2]
+                names(hospitals_1_state_output) <- c("hospital", "state")
+                
+                # If there are no hospitals found, insert NA for this state.
+                # Important: appending to a data frame requires consistency of column names.
+                if (nrow(hospitals_1_state_output) == 0) {
+                        hospitals_1_state_output <- rbind(hospitals_1_state_output, data.frame(hospital = NA,
+                                                                                               state = state))
+                }
+                
+                hospitals_out <- rbind(hospitals_out, hospitals_1_state_output)
+                
+        }
+        
+        hospitals_out
+        
 }
